23d456e6ba13c18fb9c176b8a83dadf02180c5d1
Victor Fusco Martins <victorfuscomartins@gmail.com>
2023-09-06 11:17:11 -0300

    Criando const da Lista e puxando dados

--- rotas/src/routes/EditarProduto/index.jsx
+++ rotas/src/routes/EditarProduto/index.jsx
@@ -1,9 +1,21 @@
+import { useParams, useNavigate } from "react-router-dom"
+import { listaProdutos } from "../../components/listaProdutos"
 
 export default function EditarProduto(){
+
+    const lista = listaProdutos
+    const navegacao = useNavigate()
+    const {id} = useParams()
+
+    const proc = lista.filter(prod => prod.id == id)
+    const produto = proc[0]
+
+   
 
     return(
         <main>
             <h1>Editando o Produto</h1>
+            
         </main>
     )
 }
